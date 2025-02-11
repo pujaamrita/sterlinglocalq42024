6f00df9ed0ca10597c43b5a897ba56e4da74aa60
Dhanya-SG <DHANYA.SG@ibm.com>
2025-02-11 13:44:25 +0530

    Create Test.java
    For Demo, test file
    
    Co-Authored-By: Jatla-Sri-Gowri <196012008+Jatla-Sri-Gowri@users.noreply.github.com>
    Co-Authored-By: Santhosh8874 <195996904+Santhosh8874@users.noreply.github.com>
    Co-Authored-By: pujaamrita <192089562+pujaamrita@users.noreply.github.com>

---
+++ src/com/oms/order/ue/Test.java
@@ -0,0 +1,17 @@
+package com.oms.test;
+
+
+import org.w3c.dom.Document;
+
+import com.yantra.yfs.japi.YFSEnvironment;
+
+public class Test {
+	
+	
+	public Document testMethod(YFSEnvironment env, Document doc){
+		System.out.println("Hello");
+		return doc;
+	}
+
+}
+
